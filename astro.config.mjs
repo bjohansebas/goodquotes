4658936e26e3749c3b45e32fed92b32c0a1de5e2
Sebastian Beltran <bjohansebas@gmail.com>
2024-06-15 20:45:58 -0500

    add vercel and sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  site: "https://goodquotes.vercel.app/",
 });
